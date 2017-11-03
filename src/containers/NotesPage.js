1c459dd2c79a03534fb3214204c225f96a92bac1
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-03 14:45:52 -0500

    Updated notes page with react router to add routes to new notes and show notes.

--- src/containers/NotesPage.js
+++ src/containers/NotesPage.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { fetchNotes } from '../actions';
+import NotesList from '../components/NotesList';
 import NotesShow from './NotesShow';
+import NotesNew from './NotesNew';
 
-class NotesPage extends Component {
+const MoviesPage = ({ match, notes }) =>
+  <div>
+    <NotesList notes={notes} />
+    <Route path={`${match.url}/new`} component={NotesNew} />
+    <Route path={`${match.url}/:noteId`} component={NotesShow}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Note from the list.</h3>
+    )}/>
+  </div>;
 
-  componentDidMount() {
-    this.props.fetchNotes();
-  }
-
-  render() {
-    return (
-      <div>Notes Page
-        <NotesShow />
-      </div>
-    )
-  }
-};
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     notes: state.notes
   };
 }
 
-export default connect(mapStateToProps, { fetchNotes })(NotesPage);
+export default connect(mapStateToProps)(NotesPage);
