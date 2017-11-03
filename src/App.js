65a1f64ff722eb612e30eef906f6e2cf69378476
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-03 13:43:11 -0500

    Updated app.js file to include a navbar.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+import NotesPage from '../containers/NotesPage';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">NBA News Feed</h1>
-        </header>
-        <p className="News-Feed">
-          News goes here.
-        </p>
+const App = (props) =>
+  <Router>
+    <div>
+      <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+        <NavLink style={{ marginRight: '10px' }} to="/pets">See All The Notes!</NavLink>
+        <NavLink style={{ marginRight: '10px' }} to="/pets/new">Add A Note</NavLink>
       </div>
-    );
-  }
-}
+      <Route exact path="/" render={() => <h3>Welcome to the Notes List App</h3>} />
+      <Route path="/pets" component={NotesPage} />
+    </div>
+  </Router>;
 
 export default App;
