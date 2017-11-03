dc6ad6ed7331c8ade67cca91f117fb894b5ab8d3
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-03 14:38:15 -0500

    Updated all routes in app.js.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = (props) =>
   <Router>
     <div>
       <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-        <NavLink style={{ marginRight: '10px' }} to="/pets">See All The Notes!</NavLink>
-        <NavLink style={{ marginRight: '10px' }} to="/pets/new">Add A Note</NavLink>
+        <NavLink style={{ marginRight: '10px' }} to="/notes">See All The Notes!</NavLink>
+        <NavLink style={{ marginRight: '10px' }} to="/notes/new">Add A Note</NavLink>
       </div>
       <Route exact path="/" render={() => <h3>Welcome to the NBA News Feed App</h3>} />
       <Route path="/notes" component={NotesPage} />
