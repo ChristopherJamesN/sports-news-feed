6720325ba9edda59f7af819c175e83cca2953009
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-03 13:37:34 -0500

    Update index.js to include store and provider imports.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import store from './configureStore';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
