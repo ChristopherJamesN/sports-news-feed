31d3c60cd9ae4d70107b714e830f8c9d5eb6f24b
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-03 15:02:12 -0500

    Added a link to the home page on the nav bar.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = (props) =>
       <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
         <NavLink style={{ marginRight: '10px' }} to="/notes">See All The Notes!</NavLink>
         <NavLink style={{ marginRight: '10px' }} to="/notes/new">Add A Note</NavLink>
+        <NavLink style={{ marginRight: '10px' }} to="/">Home</NavLink>
       </div>
       <Route exact path="/" render={() => <h3>Welcome to the NBA News Feed App</h3>} />
       <Route path="/notes" component={NotesPage} />
