4a3561a52aeba8a146bbc42dbbbf92320d182c2c
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-03 17:08:05 -0500

    Updated app to use react-bootstrap styling.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 import NotesPage from './containers/NotesPage';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 const App = (props) =>
   <Router>
     <div>
-      <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-        <NavLink style={{ marginRight: '10px' }} to="/notes">See All The Notes!</NavLink>
-        <NavLink style={{ marginRight: '10px' }} to="/notes/new">Add A Note</NavLink>
-        <NavLink style={{ marginRight: '10px' }} to="/">Home</NavLink>
-      </div>
-      <Route exact path="/" render={() => <h3>Welcome to the NBA News Feed App</h3>} />
-      <Route path="/notes" component={NotesPage} />
+      <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <NavLink style={{ marginRight: '10px' }} to="/notes">See All The Notes!</NavLink>
+                <NavLink style={{ marginRight: '10px' }} to="/notes/new">Add A Note</NavLink>
+                <NavLink style={{ marginRight: '10px' }} to="/">Home</NavLink>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Grid>
+      </Navbar>
+      <Jumbotron>
+          <Grid>
+            <h1>
+              <Route exact path="/" render={() => <h3>Welcome to the NBA News Feed App</h3>} />
+              <Route path="/notes" component={NotesPage} />
+            </h1>
+            <p>
+              <Button
+                bsStyle="success"
+                bsSize="large"
+                href="http://www.nba.com/news"
+                target="_blank">
+                View News
+              </Button>
+            </p>
+          </Grid>
+        </Jumbotron>
+
     </div>
   </Router>;
 
