ee9709d1e0eca48ffeabecaa9da43097cbed7482
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-03 14:57:32 -0500

    Added redux-think and applymiddleware to store.js.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/index'
+import thunk from 'redux-thunk';
 
 export function configureStore(){
-  return createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  return createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
 }
 
 export const store = configureStore()
