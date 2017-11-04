48fd958c7e50264fdbb2311ec726ed8cef209db6
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-04 13:08:32 -0500

    Update notes reducer to accept fetch news action.

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case 'FETCH_NOTES':
       return action.notes;
+    case 'FETCH_NEWS':
+      return action.news;
     case 'ADD_NOTE':
       const note = Object.assign({}, action.note, { id: state.length + 1} );
       return [ ...state, note ];
