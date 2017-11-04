55106a48ef27039d6ec6115f85d5408238fff720
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-04 13:12:54 -0500

    Added a route to the news page in App.js.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NotesPage from '../containers/NotesPage';
+import NewsPage from '../containers/NewsPage';
 import { Grid, Jumbotron } from 'react-bootstrap';
 import NavBar from './Navbar';
 
@@ -15,6 +16,7 @@ const App = (props) =>
             <h1>
               <Route exact path="/" render={() => <h3>Welcome to the NBA News Feed App</h3> } />
               <Route path="/notes" component={NotesPage} />
+              <Route path="/news" component={NewsPage} />
             </h1>
           </Grid>
       </Jumbotron>
