cbd95e67ef02565dc90c6c0aa0fa5193efb93331
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-04 18:05:05 -0500

    Moved store configuration to index.js and removed unused store.js file.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { render } from 'react-dom';
-import store from './store';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers/index'
+import thunk from 'redux-thunk';
 
 registerServiceWorker();
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
 
 render(
   <Provider store={store}>
