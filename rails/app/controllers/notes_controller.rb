435cbdfb3f787663be6ef8a268500e7f48b8649e
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-05 11:35:05 -0600

    Added a create notes function to the notes controller.

--- rails/app/controllers/notes_controller.rb
+++ rails/app/controllers/notes_controller.rb
@@ -11,4 +11,18 @@ class NotesController < ApplicationController
       )
     end
   end
+
+  def create
+    @note = Note.new(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to @note, notice: 'Note was successfully created.' }
+        format.json { render :show, status: :created, location: @note }
+      else
+        format.html { render :new }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
