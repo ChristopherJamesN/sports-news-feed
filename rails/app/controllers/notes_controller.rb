0b11884cc321848146638201f8759c8a6b577d12
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-05 11:28:16 -0600

    Added a notes controller.

---
+++ rails/app/controllers/notes_controller.rb
@@ -0,0 +1,14 @@
+class NotesController < ApplicationController
+  def index
+    q = params[:q]
+
+    if q.blank?
+      render status: 400, json: { error: 'Expected parameter `q` '}
+    else
+      render(
+        status: 200,
+        json: Note.where(["description LIKE ?", "%#{q}%"]).limit(100)
+      )
+    end
+  end
+end
