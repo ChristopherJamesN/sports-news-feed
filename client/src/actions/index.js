e64d0be7404cc09903fde5edcf1cd6236a55bc49
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-07 15:42:50 -0600

    Remove add news action creator.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -45,27 +45,6 @@ export function persistNote(name, description) {
   }
 }
 
-export function addNews(newsItem){
-  return {
-    type: 'ADD_NEWS',
-    newsItem
-  };
-};
-
-/*
-export function fetchNews() {
-  const news = [
-    {id: 1, name: 'First news article', title: 'The first news article.'},
-    {id: 2, name: 'Second news article', title: 'The second news article.'},
-    {id: 3, name: 'Third news article', title: 'The third news article.'}
-  ];
-  return {
-    type: 'FETCH_NEWS',
-    news
-  };
-}
-*/
-
 export function fetchNews() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_NEWS' });
@@ -77,12 +56,3 @@ export function fetchNews() {
       }).then(news => dispatch({ type: 'FETCH_NEWS', news }));
   };
 }
-
-/*
-export function fetchNotes(query) {
-  return fetch(`/api/note?q=${query}`, {
-    accept: 'application/json',
-  }).then(checkStatus)
-    .then(parseJSON);
-}
-*/
