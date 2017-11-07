5ad2b2f639a8eb7b743596b7dfda94fd6b56c431
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-07 15:56:28 -0600

    Added a componenet did mount method to the notes page to load a users notes.

--- client/src/containers/NotesPage.js
+++ client/src/containers/NotesPage.js
@@ -4,18 +4,28 @@ import { connect } from 'react-redux';
 import NotesList from '../components/NotesList';
 import NotesShow from './NotesShow';
 import NotesNew from './NotesNew';
+import * as actions from '../actions/actionCreators'
 
-const NotesPage = ({ match, notes }) =>
-  <div>
-    <Switch>
-      <Route path={`${match.url}/new`} component={NotesNew} />
-      <Route path={`${match.url}/:noteId`} component={NotesShow}/>
-      <Route exact path={match.url} render={() => (
-        <h3>Please select a note from the list.</h3>
-      )}/>
-    </Switch>
-    <NotesList notes={notes} />
-  </div>;
+class NotesPage extends Component {
+
+  componentDidMount = () => {
+    this.props.actions.getNotes();
+  }
+
+  render() {
+    return (
+    <div>
+      <Switch>
+        <Route path={`${match.url}/new`} component={NotesNew} />
+        <Route path={`${match.url}/:noteId`} component={NotesShow}/>
+        <Route exact path={match.url} render={() => (
+          <h3>Please select a note from the list.</h3>
+        )}/>
+      </Switch>
+      <NotesList notes={notes} />
+    </div>;
+  )}
+}
 
 const mapStateToProps = (state) => {
   return {
