63938c6fd46efd80f4f6f6da178a3c4bc4056ae7
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-07 16:11:52 -0600

    Updated notes reducer to load notes from rails API.

--- client/src/reducers/notesReducer.js
+++ client/src/reducers/notesReducer.js
@@ -2,6 +2,10 @@ export default (state = [], action) => {
   switch (action.type) {
     case 'FETCH_NOTES':
       return action.notes;
+    case 'LOADING_NOTES':
+      return {...state, loading: true}
+    case 'SHOW_NOTES':
+      return {...state, loading: false, notes: action.payload};
     case 'ADD_NOTE':
       const note = Object.assign({}, action.note, { id: state.length + 1} );
       return [ ...state, note ];
