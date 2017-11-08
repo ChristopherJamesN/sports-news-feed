794087063499444d3604ec7cc3ccec6b3cde5ffa
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-08 14:00:05 -0600

    Update notes show page with action import and handle functions.

--- client/src/containers/NotesShow.js
+++ client/src/containers/NotesShow.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {updateNote} from '../actions';
 
 const NotesShow = ({ note }) =>
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: this.state.name,
+        description: this.state.description,
+      };
+    }
+
+    handleOnSubmit = event => {
+      event.preventDefault();
+      const { updateNote, history } = this.props
+      updateNote(this.state.name, this.state.description);
+      history.push('/notes');
+    }
+
+    handleOnChange = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+
   <div className="col-md-12">
     <h2>Name: {note.name}</h2>
     <p>Description: {note.description}</p>
