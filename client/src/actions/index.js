863d55bd706b4ec2dc3ff8612e99b8237330cf34
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-08 11:07:45 -0600

    Remove fetch notes as it is no longer being used.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,18 +8,6 @@ export function addNote(note){
   };
 };
 
-export function fetchNotes() {
-  const notes = [
-    {id: 1, name: 'First', description: 'The first note.'},
-    {id: 2, name: 'Second', description: 'The second note.'},
-    {id: 3, name: 'Third', description: 'The third note.'}
-  ];
-  return {
-    type: 'FETCH_NOTES',
-    notes
-  };
-}
-
 export function getNotes() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_NOTES' })
--- client/src/reducers/notesReducer.js
+++ client/src/reducers/notesReducer.js
@@ -5,15 +5,13 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'FETCH_NOTES':
-      return action.notes;
     case 'LOADING_NOTES':
       return {...state, loading: true}
     case 'SHOW_NOTES':
       return {...state, loading: false, notes: action.payload};
     case 'ADD_NOTE':
-      const note = Object.assign({}, action.note, { id: state.length + 1} );
-      return [ ...state, note ];
+      const note = Object.assign({}, action.note, { id: state.notes.length + 1} );
+      return [ ...state, notes: state.notes.push(note) ];
     default:
       return state;
   }
