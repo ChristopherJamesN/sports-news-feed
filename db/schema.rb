b088f916c4aa2d78144f9f3755d9541a50e11ac9
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-08 16:26:26 -0600

    Migrate database.

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,22 @@
 
 ActiveRecord::Schema.define(version: 20171105164939) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "widgets", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "stock"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
