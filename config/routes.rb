14f4f2b8c0cfdee33bcbd0a6e2babe845f0ce793
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-08 15:59:07 -0600

    Update routes for Heroku deployment.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :notes, only: [:index, :create, :update]
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
