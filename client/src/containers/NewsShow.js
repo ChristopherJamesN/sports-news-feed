a32e0cb8060c4d6af989ed36841dae212e022d0f
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-08 11:35:01 -0600

    Add a target=blank to the news links to open in new tabs.

--- client/src/containers/NewsShow.js
+++ client/src/containers/NewsShow.js
@@ -5,7 +5,7 @@ const NewsShow = ({ newsItem }) =>
   <div className="col-md-12">
     <h2>Name: {newsItem.title}</h2>
     <p>Description: {newsItem.description}</p>
-    <a href={newsItem.url}>Link</a>
+    <a href={newsItem.url} target="_blank">Link</a>
   </div>;
 
 const mapStateToProps = (state, ownProps) => {
