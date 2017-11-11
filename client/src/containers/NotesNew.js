c667e3f0b8bf9b36bb6b11dc622af41d1602639a
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-11 14:33:43 -0600

    Update new note to use modal behavior.

--- client/src/containers/NotesNew.js
+++ client/src/containers/NotesNew.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {persistNote} from '../actions';
+import { Modal, Button } from 'react-bootstrap';
 
 class NotesNew extends Component {
   constructor(props) {
@@ -9,7 +10,16 @@ class NotesNew extends Component {
       name: '',
       description: '',
       link: this.props.link,
+      showModal: false,
     };
+  }
+
+  close = () => {
+    this.setState({ showModal: false });
+  }
+
+  open = () => {
+    this.setState({ showModal: true });
   }
 
   handleOnSubmit = event => {
@@ -28,38 +38,53 @@ class NotesNew extends Component {
   render() {
     return (
       <div>
-        <h2>Add a Note</h2>
-        <form onSubmit={this.handleOnSubmit} >
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              className="form-control"
-              onChange={this.handleOnChange} />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Description"
-              name="description"
-              className="form-control"
-              onChange={this.handleOnChange} />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              value={this.props.link}
-              placeholder='Link to associated article'
-              name="link"
-              className="form-control"
-              onChange={this.handleOnChange} />
-          </div>
-            <button
-              type="submit"
-              className="btn btn-primary"
-              >Add Note</button>
-        </form>
+
+      <Button
+          bsStyle="primary"
+          bsSize="large"
+          onClick={this.open}
+        >
+          Add a Note
+        </Button>
+
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add a Note Here</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form onSubmit={this.handleOnSubmit} >
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                  className="form-control"
+                  onChange={this.handleOnChange} />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Description"
+                  name="description"
+                  className="form-control"
+                  onChange={this.handleOnChange} />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  value={this.props.link}
+                  placeholder='Link to associated article'
+                  name="link"
+                  className="form-control"
+                  onChange={this.handleOnChange} />
+              </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  >Add Note</button>
+            </form>
+          </Modal.Body>
+        </Modal>
       </div>
     );
   }
