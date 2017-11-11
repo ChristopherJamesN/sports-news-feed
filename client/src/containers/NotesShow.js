4e6bb15d62620c59a7f004546554fcd9eb3d9db1
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-11 14:38:33 -0600

    Update update notes to use modal display.

--- client/src/containers/NotesShow.js
+++ client/src/containers/NotesShow.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {updateNote} from '../actions';
 import BackButton from '../components/BackButton'
-import { Panel, ListGroup, ListGroupItem, Well } from 'react-bootstrap';
+import { Panel, ListGroup, ListGroupItem, Well, Modal, Button } from 'react-bootstrap';
 
 class NotesShow extends Component {
     constructor(props) {
@@ -11,7 +11,16 @@ class NotesShow extends Component {
         name: this.props.note.name,
         description: this.props.note.description,
         link: this.props.note.link,
+        showModal: false,
       };
+    }
+
+    close = () => {
+      this.setState({ showModal: false });
+    }
+
+    open = () => {
+      this.setState({ showModal: true });
     }
 
     handleOnSubmit = event => {
@@ -45,37 +54,52 @@ class NotesShow extends Component {
         </Panel>
 
         <Well>
-          <h4>Update Note</h4>
-          <form onSubmit={this.handleOnSubmit} >
-            <div className="form-group">
-              <input
-                type="text"
-                placeholder={this.props.note.name}
-                name="name"
-                className="form-control"
-                onChange={this.handleOnChange} />
-            </div>
-            <div className="form-group">
-              <input
-                type="text"
-                placeholder={this.props.note.description}
-                name="description"
-                className="form-control"
-                onChange={this.handleOnChange} />
-            </div>
-            <div className="form-group">
-              <input
-                type="text"
-                placeholder={this.props.note.link}
-                name="link"
-                className="form-control"
-                onChange={this.handleOnChange} />
-            </div>
-              <button
-                type="submit"
-                className="btn btn-primary"
-                >Update Note</button>
-          </form>
+
+        <Button
+            bsStyle="primary"
+            bsSize="large"
+            onClick={this.open}
+          >
+            Update Note
+          </Button>
+
+          <Modal show={this.state.showModal} onHide={this.close}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add a Note Here</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <form onSubmit={this.handleOnSubmit} >
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder={this.props.note.name}
+                    name="name"
+                    className="form-control"
+                    onChange={this.handleOnChange} />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder={this.props.note.description}
+                    name="description"
+                    className="form-control"
+                    onChange={this.handleOnChange} />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder={this.props.note.link}
+                    name="link"
+                    className="form-control"
+                    onChange={this.handleOnChange} />
+                </div>
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    >Update Note</button>
+              </form>
+            </Modal.Body>
+          </Modal>
         </Well>
       </div>
     );
