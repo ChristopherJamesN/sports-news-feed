bacadc122de8f4e7a2e3a942b507bba4dcea8479
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-11 20:11:36 -0600

    Update news list to be passed a key prop.

--- client/src/components/NewsList.js
+++ client/src/components/NewsList.js
@@ -5,7 +5,7 @@ import { ListGroup, ListGroupItem } from 'react-bootstrap';
 const NewsList = ({ news }) => {
 
   const renderNews = news.map((newsItem, index) =>
-    <ListGroupItem><Link style={{ marginRight: '12px' }} key={newsItem.publishedAt} to={`/news/${newsItem.publishedAt}`}>{newsItem.title}<br></br></Link></ListGroupItem>
+    <ListGroupItem key={newsItem.id}><Link style={{ marginRight: '12px' }} key={newsItem.publishedAt} to={`/news/${newsItem.publishedAt}`}>{newsItem.title}<br></br></Link></ListGroupItem>
   );
 
   return (
