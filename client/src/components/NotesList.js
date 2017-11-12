9a3a76a1941129ecefda6a3ee05e02d0f805725c
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-12 17:41:44 -0600

    Update notes list to sort notes by id order.

--- client/src/components/NotesList.js
+++ client/src/components/NotesList.js
@@ -4,6 +4,9 @@ import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 const NotesList = ({ notes }) => {
 
+  notes.sort(function(a,b) {
+    return a.id - b.id;
+  });
   const renderNotes = notes.map((note, index) =>
     <ListGroupItem key={note.id}><Link style={{ marginRight: '12px' }} key={note.id} to={`/notes/${note.id}`}>{note.name}<br></br></Link></ListGroupItem>
   );
