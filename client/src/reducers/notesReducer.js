f3fbeb7a430aae65aa341f48a736457236a817ab
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-11 20:09:12 -0600

    Fix typo in notes reducer.

--- client/src/reducers/notesReducer.js
+++ client/src/reducers/notesReducer.js
@@ -10,7 +10,7 @@ export default (state = initialState, action) => {
     case 'SHOW_NOTES':
       return {...state, loading: false, notes: action.payload};
     case 'ADD_NOTE':
-      return Object.assign({}, state, {notes: state.notes.concat(action.notes)});
+      return Object.assign({}, state, {notes: state.notes.concat(action.payload)});
     case 'SAVING_NOTE':
       return { ...state, loading: false }
     default:
