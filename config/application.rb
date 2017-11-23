ecbe49a408ef3f119dafe1478eb0a540e00533d3
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-23 15:03:56 -0600

    Set up devise to take json requests.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module ReactReduxNbaNewsFeed
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
