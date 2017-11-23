890f32b5b6328dc35aa641047dfdd389419cfaae
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-23 15:06:18 -0600

    Setup application controller for Devise user authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :provider
+    devise_parameter_sanitizer.for(:sign_up) << :uid
+  end
+  
    def fallback_index_html
      render :file => '../client/public/index.html'
    end
