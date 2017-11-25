0212d887d8e1ede2990c32a04b574dc09436cff0
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-25 10:24:25 -0600

    Move vote button to containers since it is now stateful.

--- client/src/components/NotesList.js
+++ client/src/components/NotesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
-import VoteButton from './VoteButton';
+import VoteButton from '../containers/VoteButton';
 
 const NotesList = ({ notes }) => {
 
