75aa2b0bad33b3cfcb6ca4f3049356eddaa8fbbb
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-25 09:59:41 -0600

    Updated app to check if user is signed in.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, NavLink, Redirect } from 'react-router-dom';
 import NotesPage from '../containers/NotesPage';
@@ -6,25 +6,48 @@ import NewsPage from '../containers/NewsPage';
 import { Grid, Jumbotron } from 'react-bootstrap';
 import NavBar from './Navbar';
 import Footer from './Footer';
+import $ from 'jquery';
 
-const App = (props) =>
-  <Router>
-    <div>
-    <NavBar />
-      <Jumbotron>
-          <Grid>
-            <h2>
-              <Route path="/notes" component={NotesPage} />
-              <Route path="/news" component={NewsPage} />
+class App extends Component {
 
-            </h2>
-            <h1>
-              <Route exact path="/" render={() => <div><h2>Welcome to the Sports News Feed App</h2><p>This app pulls NBA, NFL, and other sports news, from ESPN, Fox Sports, and NFL News via NewsAPI. You can check a short description of the story and take notes as well. Links to the stories open in a new tab.</p><p><NavLink to="/news">News Feed</NavLink></p><p><NavLink to="/notes">Favorited Stories</NavLink></p></div>} />
-            </h1>
-          </Grid>
-      </Jumbotron>
-      <Footer />
-    </div>
-  </Router>;
+  constructor(props) {
+    super(props);
+    this.state = {
+      signedIn: null,
+    };
+  }
+
+  componentWillMount() {
+    $.ajax({
+      method: "GET",
+      url: "/auth/is_signed_in.json"
+    })
+    .done(function(data){
+      this.setState({ signedIn: data.signed_in });
+    }.bind(this));
+  }
+
+  render() {
+    return (
+      <Router signedIn={this.state.signedIn}>
+        <div>
+        <NavBar />
+          <Jumbotron>
+              <Grid>
+                <h2>
+                  <Route path="/notes" component={NotesPage} />
+                  <Route path="/news" component={NewsPage} />
+
+                </h2>
+                <h1>
+                  <Route exact path="/" render={() => <div><h2>Welcome to the Sports News Feed App</h2><p>This app pulls NBA, NFL, and other sports news, from ESPN, Fox Sports, and NFL News via NewsAPI. You can check a short description of the story and take notes as well. Links to the stories open in a new tab.</p><p><NavLink to="/news">News Feed</NavLink></p><p><NavLink to="/notes">Favorited Stories</NavLink></p></div>} />
+                </h1>
+              </Grid>
+          </Jumbotron>
+          <Footer />
+        </div>
+      </Router>
+  )}
+}
 
 export default App;
