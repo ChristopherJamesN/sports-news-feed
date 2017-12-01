429fdafd83df2f3316cde33955b082e5fcad8a69
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-01 13:58:17 -0600

    Add a props route component

---
+++ client/src/components/PropsRoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const PropsRoute = ({ component, ...rest }) => {
+  return (
+    <Route {...rest} render={routeProps => {
+      return renderMergedProps(component, routeProps, rest);
+    }}/>
+  );
+}
