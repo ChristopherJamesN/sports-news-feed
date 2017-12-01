6d935958d156b0c663984b1543a17186aed74246
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-01 12:00:50 -0600

    Update sign in form

--- client/src/components/auth/SignInForm.js
+++ client/src/components/auth/SignInForm.js
@@ -1,58 +1,75 @@
-var React          = require('react');
-var Functions      = require('../../utils/Functions.js');
-var _              = require('lodash');
-var $              = require('jquery');
+import React, { Component } from 'react';
+import $ from 'jquery';
+import _ from 'lodash';
 
-var SignInForm =
-  React.createClass({
-    _handleInputChange: function(ev) {
-      // Get a deep clone of the component's state before the input change.
-      var nextState = _.cloneDeep(this.state);
+class SignInForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
-      //Update the state of the component
-      nextState[ev.target.name] = ev.target.value;
+  getMetaContent = (name) => {
+    var metas = document.getElementsByTagName('meta');
 
-      // Update the component's state with the new state
-      this.setState(nextState);
-    },
-    getInitialState: function() {
-      return {
-        email: '',
-        password: ''
-      };
-    },
-    _handleSignInClick: function(e) {
-      $.ajax({
-        method: "POST",
-        url: "/users/sign_in.json",
-        data: {
-          user: {
-            email: this.state.email,
-            password: this.state.password
-          },
-          authenticity_token: Functions.getMetaContent("csrf-token")
-        }
-      })
-      .done(function(data){
-        location.reload();
-      }.bind(this));
-    },
-    render:function(){
-      return (
-          <form>
-              <input type='email'
-                name='email'
-                placeholder='email'
-                value={this.state.email}
-                onChange={this._handleInputChange} />
-              <input type='password'
-                name='password'
-                placeholder='password'
-                value={this.state.password}
-                onChange={this._handleInputChange} />
-              <input type='submit' onClick={this._handleSignInClick} defaultValue='login' />
-          </form>
-      )
+    for (var i=0; i<metas.length; i++) {
+      if (metas[i].getAttribute("name") == name) {
+        return metas[i].getAttribute("content");
+      }
     }
-  });
-module.exports = SignInForm;
+
+    return "";
+  }
+
+  _handleInputChange = (event) => {
+    // Get a deep clone of the component's state before the input change.
+    var nextState = _.cloneDeep(this.state);
+
+    //Update the state of the component
+    nextState[event.target.name] = event.target.value;
+
+    // Update the component's state with the new state
+    this.setState(nextState);
+  }
+
+  _handleSignInClick = () => {
+    $.ajax({
+      method: "POST",
+      url: "/users/sign_in.json",
+      data: {
+        user: {
+          email: this.state.email,
+          password: this.state.password
+        },
+        authenticity_token: this.getMetaContent("csrf-token")
+      }
+    })
+    .done(function(data){
+      window.reload();
+    }.bind(this));
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+            <input type='email'
+              name='email'
+              placeholder='email'
+              value={this.state.email}
+              onChange={this._handleInputChange} />
+            <input type='password'
+              name='password'
+              placeholder='password'
+              value={this.state.password}
+              onChange={this._handleInputChange} />
+            <input type='submit' onClick={this._handleSignInClick} defaultValue='login' />
+        </form>
+      </div>
+    );
+  }
+};
+
+export default SignInForm;
