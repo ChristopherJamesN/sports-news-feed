3ea261051a388c0fb3b69ae1050ab0d3c21a6df0
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-01 12:04:14 -0600

    Update sign up form

--- client/src/components/auth/SignUpForm.js
+++ client/src/components/auth/SignUpForm.js
@@ -1,77 +1,94 @@
-var React          = require('react');
-var _              = require('lodash');
-var Functions      = require('../../utils/Functions.js');
+import React, { Component } from 'react';
+import _ from 'lodash';
 
-var SignUpForm =
-  React.createClass({
-    _handleInputChange: function(ev) {
-      // Get a deep clone of the component's state before the input change.
-      var nextState = _.cloneDeep(this.state);
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: '',
+      name: '',
+    };
+  }
 
-      //Update the state of the component
-      nextState[ev.target.name] = ev.target.value;
+  getMetaContent = (name) => {
+    var metas = document.getElementsByTagName('meta');
 
-      // Update the component's state with the new state
-      this.setState(nextState);
-    },
-    getInitialState: function() {
-      return {
-        email: '',
-        password: '',
-        password_confirmation: '',
-        name: ''
-      };
-    },
-    _handleRegistrationClick: function(e) {
-      $.ajax({
-        method: "POST",
-        url: "/users.json",
-        data: {
-          user: {
-            email: this.state.email,
-            uid: this.state.email,
-            password: this.state.password,
-            password_confirmation: this.state.password_confirmation,
-            name: this.state.name,
-            provider: "email"
-          },
-          authenticity_token: Functions.getMetaContent("csrf-token")
-        }
-      })
-      .done(function(data){
-        location.reload();
-      }.bind(this));
-    },
-    render:function(){
-      return (
-          <form>
-              <input type='text'
-                name='name'
-                placeholder='name'
-                value={this.state.name}
-                onChange={this._handleInputChange} />
-
-              <input type='email'
-                name='email'
-                placeholder='email'
-                value={this.state.email}
-                onChange={this._handleInputChange} />
-
-              <input type='password'
-                name='password'
-                placeholder='password'
-                value={this.state.password}
-                onChange={this._handleInputChange} />
-
-              <input type='password'
-                name='password_confirmation'
-                placeholder='re-type password'
-                value={this.state.password_confirmation}
-                onChange={this.handleInputChange} />
-            </div>
-            <input onClick={this._handleRegistrationClick} defaultValue="sign up"/>
-          </form>
-      )
+    for (var i=0; i<metas.length; i++) {
+      if (metas[i].getAttribute("name") == name) {
+        return metas[i].getAttribute("content");
+      }
     }
-  });
-module.exports = SignUpForm;
+
+    return "";
+  }
+
+  _handleInputChange = (event) => {
+    // Get a deep clone of the component's state before the input change.
+    var nextState = _.cloneDeep(this.state);
+
+    //Update the state of the component
+    nextState[event.target.name] = event.target.value;
+
+    // Update the component's state with the new state
+    this.setState(nextState);
+  }
+
+  _handleRegistrationClick = () => {
+    $.ajax({
+      method: "POST",
+      url: "/users.json",
+      data: {
+        user: {
+          email: this.state.email,
+          uid: this.state.email,
+          password: this.state.password,
+          password_confirmation: this.state.password_confirmation,
+          name: this.state.name,
+          provider: "email"
+        },
+        authenticity_token: Functions.getMetaContent("csrf-token")
+      }
+    })
+    .done(function(data){
+      location.reload();
+    }.bind(this));
+  },
+
+  render() {
+    return (
+      <div>
+      <form>
+          <input type='text'
+            name='name'
+            placeholder='name'
+            value={this.state.name}
+            onChange={this._handleInputChange} />
+
+          <input type='email'
+            name='email'
+            placeholder='email'
+            value={this.state.email}
+            onChange={this._handleInputChange} />
+
+          <input type='password'
+            name='password'
+            placeholder='password'
+            value={this.state.password}
+            onChange={this._handleInputChange} />
+
+          <input type='password'
+            name='password_confirmation'
+            placeholder='re-type password'
+            value={this.state.password_confirmation}
+            onChange={this.handleInputChange} />
+        </div>
+        <input onClick={this._handleRegistrationClick} defaultValue="sign up"/>
+      </form>
+      </div>
+    );
+  }
+};
+
+export default SignUpForm;
