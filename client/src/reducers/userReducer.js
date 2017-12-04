5ed235630aaadf9c3ae37fcf136b29dda51eef5a
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-04 11:58:39 -0600

    Update userReducer to conform to JWT authentication

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,18 +1,35 @@
 const initialState = {
-  loading: '',
-  user: ''
+  id: '',
+  username: '',
+  name: '',
+  email: '',
+  isLoggedIn: false,
+  loading: false
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'LOADING_USER':
-      return {...state, loading: true}
-    case 'SHOW_USER':
-      return {...state, loading: false, user: action.user}
-    case 'SAVING_USER':
-      return {...state, loading: true}
-    case 'SIGN_OUT':
-      return state;
+    case 'RETURN_JWT':
+      return { loading: false }
+    case 'CURRENT_USER':
+      const user = Object.assign({}, state, action.payload);
+      return {
+        id: user.id,
+        username: user.username,
+        name: user.name,
+        email: user.email,
+        isLoggedIn: true,
+        loading: false
+      }
+    case 'LOGGED_OUT':
+      return {
+        loading: false,
+        isLoggedIn: false,
+        id: null,
+        username: null,
+        name: null,
+        email: null
+      }
     default:
       return state;
   }
