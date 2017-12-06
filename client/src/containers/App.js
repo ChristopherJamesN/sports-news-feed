573db912ddfe458b0d0f8e41d0e0c8a789ae23ee
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-06 10:11:11 -0600

    Update styling of home page

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -29,7 +29,7 @@ class App extends Component {
                   <Route path="/signup" component={SignUpForm} />
                 </h2>
                 <h1>
-                  <Route exact path="/" render={() => <div><h2>Welcome to the Sports News Feed App</h2><p>This app pulls NBA, NFL, and other sports news, from ESPN, Fox Sports, and NFL News via NewsAPI. You can read a short description, favorite, and comment on stories. Links to the stories open in a new tab.</p><p><NavLink to="/news">News Feed</NavLink></p><p><NavLink to="/notes">Favorited Stories</NavLink></p></div>} />
+                  <Route exact path="/" render={() => <div><h2>Welcome to the Sports News Feed App</h2><p>This app pulls NBA, NFL, and other sports news, from ESPN, Fox Sports, and NFL News via NewsAPI. You can read a short description, favorite, and take notes on stories. Links to the stories open in a new tab. Sign in or sign up to favorite and take notes on stories.</p><h3><NavLink to="/news">News Feed</NavLink></h3></div>} />
                 </h1>
               </Grid>
           </Jumbotron>
