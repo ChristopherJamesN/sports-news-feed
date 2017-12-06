cdb95701e383a6f262d82d9a8cb484a8f94ec9b8
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-06 12:42:37 -0600

    Update package.json and remove unused navbar components

--- client/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NavBarSignedIn from './NavBarSignedIn';
-import NavBarSignedOut from './NavBarSignedOut';
-
-const NavBar = ({ loggedIn }) => {
-  return ( loggedIn? (<NavBarSignedIn />) : (<NavBarSignedOut />))
-}
-
-export default NavBar;
