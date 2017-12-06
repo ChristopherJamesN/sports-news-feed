0f194d6a7d46040b3e95eea3f560cfd36cfb814c
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-06 12:22:44 -0600

    Add a navigation container

---
+++ client/src/containers/Navigation.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavbarBrand, NavItem, NavLink } from 'reactstrap';
+import { connect } from 'react-redux';
+import UserDropdown from '../components/UserDropdown';
+
+class Navigation extends Component {
+
+  render() {
+    return (
+      <Navbar className='sticky-top d-flex flex-row' color='faded' light>
+        <NavbarBrand className='mr-auto' href='/'>Home</NavbarBrand>
+        <Nav className='d-inline-flex flex-row' navbar>
+          <NavItem className='mr-5 mt-2'>
+            <NavLink className='d-inline align-bottom' href='/news'>News Feed</NavLink>
+          </NavItem>
+          <NavItem className='ml-1 mr-3 mt-2'>
+            {!this.props.isLoggedIn ? (
+              <NavLink className='d-inline text-bottom' href='/login'>Login</NavLink>
+             ) : (
+              <UserLinkOptions className='mt-0'  />
+             )}
+          </NavItem>
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { isLoggedIn: state.userReducer.isLoggedIn }
+}
+
+export default connect(mapStateToProps)(Navigation);
