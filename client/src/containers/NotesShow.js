0faa7f833ab9273ed9d04c2851c93befc71a1671
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-07 15:44:39 -0600

    Update styling of notes show to use react strap

--- client/src/containers/NotesShow.js
+++ client/src/containers/NotesShow.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import {updateNote} from '../actions';
 import BackButton from '../components/BackButton';
 import CommentsNew from './CommentsNew';
-import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 import { bindActionCreators } from 'redux';
 
 class NotesShow extends Component {
@@ -25,19 +25,20 @@ class NotesShow extends Component {
           <BackButton />
         </div>
         <br></br>
-        <Panel header={this.props.note.name} bsStyle="primary">
           <ListGroup fill>
             <ListGroupItem>
-              <p>Description: {this.props.note.description}</p>
+              <h1>{this.props.note.name}</h1>
             </ListGroupItem>
-            <ListGroupItem>
-              {this.props.note.comments.map((comment, index) => <p key={index}>{comment}</p>)}
-            </ListGroupItem>
-            <ListGroupItem>
-              <a href={this.props.note.link} target="_blank">Link to Associated Article</a>
-            </ListGroupItem>
+              <ListGroupItem>
+                <h2>Description: {this.props.note.description}</h2>
+              </ListGroupItem>
+              <ListGroupItem>
+                {this.props.note.comments.map((comment, index) => <p key={index}>{comment}</p>)}
+              </ListGroupItem>
+              <ListGroupItem>
+                <a href={this.props.note.link} target="_blank">Link to Associated Article</a>
+              </ListGroupItem>
           </ListGroup>
-        </Panel>
         <br></br>
         <div>
           <CommentsNew id={this.props.note.id} name={this.props.note.name} description={this.props.note.description} link={this.props.note.link}/>
