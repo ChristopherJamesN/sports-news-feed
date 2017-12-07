e651b6ff66db0cf806942b48501bbc034c575f63
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-07 17:20:23 -0600

    Add user_id into notes table

--- db/migrate/20171105164939_create_notes.rb
+++ db/migrate/20171105164939_create_notes.rb
@@ -3,6 +3,7 @@ class CreateNotes < ActiveRecord::Migration[5.1]
     create_table :notes do |t|
       t.string :name
       t.string :description
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171204164648) do
   create_table "notes", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "link"
