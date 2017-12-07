71eb4741dff9fb1983ca0f25bb4caec7518a7fb5
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-07 17:10:27 -0600

    Update notes model and controller to store notes per user.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,29 +2,42 @@ class NotesController < ApplicationController
   #before_action :authenticate_user
 
   def index
-    @notes = Note.all
-    render json: @notes
+    if current_user
+      user = User.find_by(:id => params[:user_id].to_i)
+      @notes = user.notes.all
+      render json: @notes, status: 200
+    else
+      redirect_to "/login"
+      render json: { errors: @notes.errors.full_messages }, status: 500
+    end
   end
 
   def create
     @note = Note.new(note_params)
     if @note.save
       render json: @note, status: 201
+    else
+      render json: { errors: @note.errors.full_messages }, status: 500
     end
   end
 
   def update
-    @note = Note.find(params[:id])
-    @note.update(comments: @note.comments.push(params[:note][:comments]))
-    @note.save
-    render json: @note
+    if current_user
+      @note = Note.find(params[:id])
+      @note.update(comments: @note.comments.push(params[:note][:comments]))
+      @note.save
+      render json: @note
+    else
+      redirect "/login"
+      render json: { errors: @notes.errors.full_messages }, status: 500
+    end
   end
 
 
 
   private
   def note_params
-    params.require(:note).permit(:name, :description, :link, :comments)
+    params.require(:note).permit(:name, :description, :link, :comments, :user_id)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :notes
 
   validates :email, presence: true
   validates :email, uniqueness: true
