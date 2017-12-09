5666bc88a63e03d8614f4688ee9b476cfccf7f2c
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-08 20:12:24 -0600

    Update styling of home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :notes
 
-  validates :email, presence: true
+  validates :email, :password, :password_confirmation, presence: true
   validates :email, uniqueness: true
 end
--- client/src/containers/Homepage.js
+++ client/src/containers/Homepage.js
@@ -4,9 +4,9 @@ import IntroText from '../components/IntroText'
 
 const Homepage = (props) => {
     return(
-      <Container className='w-100 display-height p-0'>
+      <div>
         <IntroText />
-      </Container>
+      </div>
     )
   }
 
