5a07778c34fdf0b449c3290b7c54511fe50737ed
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-11 15:04:39 -0600

    Add alerts to let user know if they are signed in or out.

--- client/src/containers/Homepage.js
+++ client/src/containers/Homepage.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import IntroText from '../components/IntroText'
+import { Alert } from 'reactstrap';
+import { connect } from 'react-redux';
 
-const Homepage = (props) => {
-    return(
-      <div>
-        <IntroText />
-      </div>
-    )
+class Homepage extends Component {
+    render() {
+      return(
+        <div>
+          <Alert color="info" isOpen={this.props.isLoggedIn} >You are logged in.</Alert>
+          <Alert color="info" isOpen={!this.props.isLoggedIn} >You are logged out.</Alert>
+          <IntroText />
+        </div>
+      )}
+  }
+
+  const mapStateToProps = (state) => {
+    return {
+      isLoggedIn: state.userReducer.isLoggedIn
+    };
   }
 
 
-export default Homepage;
+export default connect(mapStateToProps)(Homepage);
