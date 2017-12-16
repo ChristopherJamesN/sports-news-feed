77d6a48ba6cc8d388fb92cc5c37493e1bb9706fb
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-16 14:36:52 -0600

    Add sticky top and toggler to navbar

--- client/src/containers/Navigation.js
+++ client/src/containers/Navigation.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'reactstrap';
+import { Collapse, Navbar, Nav, NavItem, NavbarToggler } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class Navigation extends Component {
 
+  constructor(props) {
+      super(props);
+
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
+
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+
   render() {
     return (
-      <Navbar color='faded' light expand="md">
+      <Navbar color='light' light expand="md" className="sticky-top">
+      <NavbarToggler onClick={this.toggle} />
+      <Collapse isOpen={this.state.isOpen} navbar>
         <Nav>
         <li>
           <NavLink to='/' style={{ textDecoration: 'none' }}>&nbsp; Home &nbsp;</NavLink>
@@ -37,6 +54,7 @@ class Navigation extends Component {
           )}
 
         </Nav>
+      </Collapse>
       </Navbar>
     );
   }
