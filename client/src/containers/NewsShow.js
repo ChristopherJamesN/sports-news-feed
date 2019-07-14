ea7cf111aa99263f0c888d65a17a0cb578767cbf
ChristopherJamesN <nady.christopher@gmail.com>
2019-07-14 15:59:29 -0500

    Update dependencies.

--- client/src/containers/NewsShow.js
+++ client/src/containers/NewsShow.js
@@ -1,38 +1,54 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import NotesNew from './NotesNew';
-import BackButton from '../components/BackButton'
+import BackButton from '../components/BackButton';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
-const NewsShow = ({ newsItem }) =>
+const NewsShow = ({ newsItem }) => (
   <div>
     <div>
       <BackButton />
     </div>
-    <br></br>
+    <br />
     <ListGroup>
-        <ListGroupItem><h3> {newsItem.title} </h3></ListGroupItem>
+      <ListGroupItem>
+        <h3> {newsItem.title} </h3>
+      </ListGroupItem>
     </ListGroup>
     <ListGroup>
-        <ListGroupItem><p>{newsItem.description}</p></ListGroupItem>
+      <ListGroupItem>
+        <p>{newsItem.description}</p>
+      </ListGroupItem>
     </ListGroup>
     <ListGroup>
-        <ListGroupItem><a href={newsItem.url} target="_blank">Link to Full Story</a></ListGroupItem>
+      <ListGroupItem>
+        <a href={newsItem.url} target="_blank">
+          Link to Full Story
+        </a>
+      </ListGroupItem>
     </ListGroup>
-    <br></br>
+    <br />
     <div>
-      <NotesNew link={newsItem.url} name={newsItem.title} description={newsItem.description}></NotesNew>
+      <NotesNew
+        link={newsItem.url}
+        name={newsItem.title}
+        description={newsItem.description}
+      />
     </div>
-  </div>;
+  </div>
+);
 
 const mapStateToProps = (state, ownProps) => {
   // eslint-disable-next-line
-  const newsItem = state.newsReducer.news.find(newsItem => newsItem.publishedAt == ownProps.match.params.newsItemPublishedAt)
+  const newsItem = state.newsReducer.news.find(
+    newsItem =>
+      newsItem.publishedAt == ownProps.match.params.newsItemPublishedAt
+  );
 
   if (newsItem) {
-    return { newsItem }
+    return { newsItem };
   } else {
-    return { newsItem: {} }
+    return { newsItem: {} };
   }
 };
 
