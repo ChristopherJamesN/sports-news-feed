5466738c76c567f764bc13b12a3c4e962c054ae2
ChristopherJamesN <nady.christopher@gmail.com>
2020-06-13 14:12:38 -0500

    Handle empty news in NewsList.js.

--- client/src/components/NewsList.js
+++ client/src/components/NewsList.js
@@ -3,16 +3,33 @@ import { Link } from 'react-router-dom';
 import { ListGroup } from 'reactstrap';
 
 const NewsList = ({ news }) => {
-
-  const renderNews = news.filter(newsItem => newsItem.title).map((newsItem, index) =>
-      <ListGroup key={index}><Link key={index} className='list-group-item list-group-item-action' to={`/news/${newsItem.publishedAt}`}>{newsItem.title || "Story not found"}</Link></ListGroup>
+  let renderNews = (
+    <ListGroup key={1}>
+      <Link
+        key={1}
+        className="list-group-item list-group-item-action"
+        to={`/news`}
+      >
+        {'No stories found'}
+      </Link>
+    </ListGroup>
   );
-
-  return (
-    <div>
-      {renderNews}
-    </div>
-  );
+  if (news) {
+    renderNews = news
+      .filter((newsItem) => newsItem.title)
+      .map((newsItem, index) => (
+        <ListGroup key={index}>
+          <Link
+            key={index}
+            className="list-group-item list-group-item-action"
+            to={`/news/${newsItem.publishedAt}`}
+          >
+            {newsItem.title || 'Story not found'}
+          </Link>
+        </ListGroup>
+      ));
+  }
+  return <div>{renderNews}</div>;
 };
 
 export default NewsList;
