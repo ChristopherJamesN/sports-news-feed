e0fcfeda24ef99e3c947e23e8fa7a3638f0e7b2e
ChristopherJamesN <nady.christopher@gmail.com>
2020-06-13 15:23:14 -0500

    Move compiled assests to public directory in root of project.

---
+++ public/precache-manifest.434d18889ae6c9cda1f24f102cc2d0fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8568de61d503fa8348aab7ae1c2d9233",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13505445749b4532bc13",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "316d6d7833511434b8eb",
+    "url": "/static/css/main.72777d42.chunk.css"
+  },
+  {
+    "revision": "13505445749b4532bc13",
+    "url": "/static/js/2.7222e5a3.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/static/js/2.7222e5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316d6d7833511434b8eb",
+    "url": "/static/js/main.25b438bb.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
