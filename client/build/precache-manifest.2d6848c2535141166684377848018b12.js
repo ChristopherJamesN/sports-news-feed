549ee991f575530547a598d99a5ce6868d1ba2b1
ChristopherJamesN <nady.christopher@gmail.com>
2020-06-13 14:21:38 -0500

    Compile static assests for deployment.

---
+++ client/build/precache-manifest.2d6848c2535141166684377848018b12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49879ae5acba1a4af7a140d2d1fe9ec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13505445749b4532bc13",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "823f1f93441486115dc5",
+    "url": "/static/css/main.72777d42.chunk.css"
+  },
+  {
+    "revision": "13505445749b4532bc13",
+    "url": "/static/js/2.7222e5a3.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/static/js/2.7222e5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "823f1f93441486115dc5",
+    "url": "/static/js/main.eff275f3.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
