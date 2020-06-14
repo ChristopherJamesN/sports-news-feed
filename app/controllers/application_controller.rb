a20e63f077f6399d4523412934149d574d02db8d
ChristopherJamesN <nady.christopher@gmail.com>
2020-06-14 17:43:00 -0500

    Extract cache fetching into seperate methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,27 +7,39 @@ class ApplicationController < ActionController::Base
      render :file => '../client/public/index.html'
    end
 
-   # To-do: Would like to cache these responses and maybe only pull new stories every 12
-   # hours or so. With the free API plan, only 500 requests per month are allowed.
-
    def retrieve_nfl_news
+    res = fetch_nfl_news_from_cache_or_api()
+    render json: res, status: 200
+   end
+
+   def fetch_nfl_news_from_cache_or_api
     Rails.cache.fetch("/retrieve_nfl_news", expires_in: 12.hours) do
-      res = Net::HTTP.get(URI.parse("https://newsapi.org/v1/articles?source=nfl-news&sortBy=top&apiKey=#{ENV['APIKEY']}"))
-      render json: res, status: 200
+      puts("Fetching nfl news from news api")
+      res = Net::HTTP.get(URI.parse("https://newsapi.org/v1/articles?source=nfl-news&sortBy=top&apiKey=#{ENV['APIKEY']}")) 
     end
    end
 
    def retrieve_espn_news
+    res = fetch_espn_news_from_cache_or_api()
+    render json: res, status: 200
+   end
+
+   def fetch_espn_news_from_cache_or_api
     Rails.cache.fetch("/retrieve_espn_news", expires_in: 12.hours) do
-      res = Net::HTTP.get(URI.parse("https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=#{ENV['APIKEY']}"))
-      render json: res, status: 200
+      puts("Fetching espn news from news api")
+      res = Net::HTTP.get(URI.parse("https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=#{ENV['APIKEY']}"))  
     end
    end
 
    def retrieve_fox_sports_news
+    res = fetch_fox_sports_news_from_cache_or_api()
+    render json: res, status: 200
+   end
+
+   def fetch_fox_sports_news_from_cache_or_api
     Rails.cache.fetch("/retrieve_fox_sports_news", expires_in: 12.hours) do
-      res = Net::HTTP.get(URI.parse("https://newsapi.org/v1/articles?source=fox-sports&sortBy=top&apiKey=#{ENV['APIKEY']}"))
-      render json: res, status: 200
+      puts("Fetching fox sports news from news api")
+      res = Net::HTTP.get(URI.parse("https://newsapi.org/v1/articles?source=fox-sports&sortBy=top&apiKey=#{ENV['APIKEY']}"))   
     end
    end
 end
