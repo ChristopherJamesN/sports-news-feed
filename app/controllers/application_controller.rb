5be482cda3a66a0add58bb3e1ea24f192760b8c3
ChristopherJamesN <nady.christopher@gmail.com>
2020-06-14 10:43:54 -0500

    Add to-do comment on caching news responses in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,9 @@ class ApplicationController < ActionController::Base
      render :file => '../client/public/index.html'
    end
 
+   # To-do: Would like to cache these responses and maybe only pull new stories every 12
+   # hours or so. With the free API plan, only 500 requests per month are allowed.
+
    def retrieve_nfl_news
     res = Net::HTTP.get(URI.parse("https://newsapi.org/v1/articles?source=nfl-news&sortBy=top&apiKey=#{ENV['APIKEY']}"))
     render json: res, status: 200
