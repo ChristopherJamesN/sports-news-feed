f33873a0c096d1c3edc0af080dc214a1a0e7b16f
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-22 17:38:08 -0500

    Update formatting on NotesList component.

--- client/src/components/NotesList.js
+++ client/src/components/NotesList.js
@@ -3,19 +3,22 @@ import { Link } from 'react-router-dom';
 import { ListGroup } from 'reactstrap';
 
 const NotesList = ({ notes }) => {
+  notes.sort(function (a, b) {
+    return a.id - b.id;
+  });
+  const renderNotes = notes.map((note, index) => (
+    <ListGroup key={note.id}>
+      <Link
+        key={note.id}
+        className="list-group-item list-group-item-action"
+        to={`/notes/${note.id}`}
+      >
+        {note.name}
+      </Link>
+    </ListGroup>
+  ));
 
-    notes.sort(function(a,b) {
-      return a.id - b.id;
-    });
-    const renderNotes = notes.map((note, index) =>
-      <ListGroup key={note.id}><Link key={note.id} className="list-group-item list-group-item-action" to={`/notes/${note.id}`}>{note.name}</Link></ListGroup>
-    );
-
-  return (
-    <div>
-      {renderNotes}
-    </div>
-  );
+  return <div>{renderNotes}</div>;
 };
 
 export default NotesList;
