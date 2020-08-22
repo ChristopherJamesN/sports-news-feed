60a19298012e88ac6cf238400846d1790069204a
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-22 15:37:52 -0500

    Add endpoint to fetch all sports news from.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,11 @@ class ApplicationController < ActionController::Base
     render json: res, status: 200
    end
 
+   def retrieve_sports_news
+    res = fetch_sports_news_from_cache_or_api()
+    render json: res, status: 200
+   end
+
    def fetch_espn_news_from_cache_or_api
     Rails.cache.fetch("/retrieve_espn_news", expires_in: 6.hours) do
       res = Net::HTTP.get(URI.parse("https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=#{ENV['APIKEY']}"))  
@@ -37,6 +42,12 @@ class ApplicationController < ActionController::Base
    def fetch_fox_sports_news_from_cache_or_api
     Rails.cache.fetch("/retrieve_fox_sports_news", expires_in: 6.hours) do
       res = Net::HTTP.get(URI.parse("https://newsapi.org/v1/articles?source=fox-sports&sortBy=top&apiKey=#{ENV['APIKEY']}"))   
+    end
+   end
+
+   def fetch_sports_news_from_cache_or_api
+    Rails.cache.fetch("/retrieve_sports_news", expires_in: 6.hours) do
+      res = Net::HTTP.get(URI.parse("https://newsapi.org/v2/top-headlines?sources=fox-sports,espn,nfl-news?&apiKey=#{ENV['APIKEY']}"))
     end
    end
 end
