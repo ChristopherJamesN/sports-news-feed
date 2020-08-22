69ada7064bcc2031a31588ba5155caef8cc2b2bf
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-22 17:36:03 -0500

    Update IntroText text.

--- client/src/components/IntroText.js
+++ client/src/components/IntroText.js
@@ -3,19 +3,32 @@ import { NavLink } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 const IntroText = () => {
-    return(
-      <div>
-        <Breadcrumb>
-          <BreadcrumbItem><h2>Welcome to the Sports News Feed App</h2></BreadcrumbItem>
-        </Breadcrumb>
-        <Breadcrumb>
-          <BreadcrumbItem><p>This app pulls NBA, NFL, and other sports news, from ESPN, Fox Sports, and NFL News via NewsAPI. You can read a short description, favorite, and take notes on stories. Links to the stories open in a new tab. Sign in or sign up to favorite and take notes on stories.</p></BreadcrumbItem>
-        </Breadcrumb>
-        <Breadcrumb>
-          <BreadcrumbItem><h3><NavLink to="/news">News Feed</NavLink></h3></BreadcrumbItem>
-        </Breadcrumb>
-      </div>
-    )
+  return (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <h2>Welcome to the Sports News Feed App</h2>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <p>
+            This app retrieves sports new via NewsAPI. You can read a short
+            description, favorite, and take notes on stories. Links to the
+            stories open in a new tab. Sign in or sign up to favorite and take
+            notes on stories.
+          </p>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <h3>
+            <NavLink to="/news">News Feed</NavLink>
+          </h3>
+        </BreadcrumbItem>
+      </Breadcrumb>
+    </div>
+  );
 };
 
 export default IntroText;
