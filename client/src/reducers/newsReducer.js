0d79536952ddb324eb23fc56510ca77f913c24bd
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-22 15:46:37 -0500

    Update newsReducer to handle new ADD_SPORTS_NEWS case.

--- client/src/reducers/newsReducer.js
+++ client/src/reducers/newsReducer.js
@@ -1,18 +1,14 @@
 const initialState = {
   loading: '',
-  news: []
-}
+  news: [],
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'LOADING_NEWS':
-      return {...state, loading: true}
-    case 'FETCH_NEWS':
-      return {...state, loading: false, news: action.news}
-    case 'ADD_FOX_SPORTS':
-      return Object.assign({}, state, {news: state.news.concat(action.news)});
-    case 'ADD_NFL_NEWS':
-      return Object.assign({}, state, {news: state.news.concat(action.news)});
+      return { ...state, loading: true };
+    case 'ADD_SPORTS_NEWS':
+      return { ...state, loading: false, news: action.news };
     default:
       return state;
   }
