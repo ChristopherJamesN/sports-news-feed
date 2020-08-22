851c46926df2df7b3efd931df2c7d6c78ad4f8cf
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-22 15:44:01 -0500

    fetchSportsNews on NewsPage component mount.

--- client/src/containers/NewsPage.js
+++ client/src/containers/NewsPage.js
@@ -3,46 +3,48 @@ import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import NewsList from '../components/NewsList';
 import NewsShow from './NewsShow';
-import { fetchNews, fetchFoxSports, fetchNFLNews } from '../actions';
+import { fetchSportsNews } from '../actions';
 import { bindActionCreators } from 'redux';
 
 class NewsPage extends Component {
-
   componentDidMount() {
-    this.props.fetchNews();
-    this.props.fetchNFLNews();
-    this.props.fetchFoxSports();
+    this.props.fetchSportsNews();
   }
 
   render() {
     return (
       <div>
         <Switch>
-          <Route path={`${this.props.match.url}/:newsItemPublishedAt`} component={NewsShow}/>
-          <Route exact path={this.props.match.url} render={() => (
-            <div>
-              <h3>Select a news item from the list to see more details.</h3>
-              <NewsList news={this.props.news} />
-            </div>
-          )}/>
+          <Route
+            path={`${this.props.match.url}/:newsItemPublishedAt`}
+            component={NewsShow}
+          />
+          <Route
+            exact
+            path={this.props.match.url}
+            render={() => (
+              <div>
+                <h3>Select a news item from the list to see more details.</h3>
+                <NewsList news={this.props.news} />
+              </div>
+            )}
+          />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    news: state.newsReducer.news
+    news: state.newsReducer.news,
   };
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchNews: bindActionCreators(fetchNews, dispatch),
-    fetchFoxSports: bindActionCreators(fetchFoxSports, dispatch),
-    fetchNFLNews: bindActionCreators(fetchNFLNews, dispatch)
-   }
+    fetchSportsNews: bindActionCreators(fetchSportsNews, dispatch),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewsPage);
