a39d69f8a5014d32efba3dddf95bb895369f5442
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-23 09:56:01 -0500

    Update formatting on userReducer.

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -4,13 +4,13 @@ const initialState = {
   isLoggedIn: false,
   loading: false,
   signupError: false,
-  signinError: false
-}
+  signinError: false,
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'RETURN_JWT':
-      return { loading: false }
+      return { loading: false };
     case 'CURRENT_USER':
       const user = Object.assign({}, state, action.payload);
       return {
@@ -19,8 +19,8 @@ export default (state = initialState, action) => {
         isLoggedIn: true,
         loading: false,
         signupError: false,
-        signinError: false
-      }
+        signinError: false,
+      };
     case 'LOGGED_OUT':
       return {
         loading: false,
@@ -28,26 +28,26 @@ export default (state = initialState, action) => {
         id: null,
         email: null,
         signupError: false,
-        signinError: false
-      }
-      case 'INVALID_SIGNUP':
-        return {
-          loading: false,
-          isLoggedIn: false,
-          id: null,
-          email: null,
-          signupError: true,
-          signinError: false
-        }
-        case 'INVALID_SIGNIN':
-          return {
-            loading: false,
-            isLoggedIn: false,
-            id: null,
-            email: null,
-            signupError: false,
-            signinError: true
-          }
+        signinError: false,
+      };
+    case 'INVALID_SIGNUP':
+      return {
+        loading: false,
+        isLoggedIn: false,
+        id: null,
+        email: null,
+        signupError: true,
+        signinError: false,
+      };
+    case 'INVALID_SIGNIN':
+      return {
+        loading: false,
+        isLoggedIn: false,
+        id: null,
+        email: null,
+        signupError: false,
+        signinError: true,
+      };
     default:
       return state;
   }
