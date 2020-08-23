ad5dacb09fb96a0f7194bc828fd1c79f3faa2be2
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-23 10:02:28 -0500

    Organize imports in store.js.

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,16 @@
-import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import loading from './reducers/loading';
-import userReducer from './reducers/userReducer';
-import notesReducer from './reducers/notesReducer';
-import newsReducer from './reducers/newsReducer';
 import { loadState, saveState } from './localStorage';
+import loading from './reducers/loading';
+import newsReducer from './reducers/newsReducer';
+import notesReducer from './reducers/notesReducer';
+import userReducer from './reducers/userReducer';
 
 const reducers = combineReducers({
   userReducer,
   loading,
   notesReducer,
-  newsReducer
+  newsReducer,
 });
 
 const persistedState = loadState();
@@ -22,16 +22,14 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducers,
   persistedState,
-  composeEnhancers(
-    applyMiddleware(...middleware)
-  )
-)
+  composeEnhancers(applyMiddleware(...middleware))
+);
 
 store.subscribe(() => {
   saveState({
     userReducer: store.getState().userReducer,
     notesReducer: store.getState().notesReducer,
-    newsReducer: store.getState().newsReducer
+    newsReducer: store.getState().newsReducer,
   });
 });
 export default store;
