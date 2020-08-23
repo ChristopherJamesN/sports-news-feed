e8e221f2ae14a10751303e70350e35c69dbb3d2a
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-23 09:55:35 -0500

    Update formatting on notesReducer.

--- client/src/reducers/notesReducer.js
+++ client/src/reducers/notesReducer.js
@@ -1,20 +1,24 @@
 const initialState = {
   loading: '',
-  notes: []
-}
+  notes: [],
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'LOADING_NOTES':
-      return {...state, loading: true}
+      return { ...state, loading: true };
     case 'SHOW_NOTES':
-      return {...state, loading: false, notes: action.payload};
+      return { ...state, loading: false, notes: action.payload };
     case 'SAVING_NOTE':
-      return { ...state, loading: false }
+      return { ...state, loading: false };
     case 'DELETING_NOTE':
-      return { ...state, loading: false }
+      return { ...state, loading: false };
     case 'ADD_NOTES':
-      return{...state, loading: false, notes: state.notes.concat(action.payload)};
+      return {
+        ...state,
+        loading: false,
+        notes: state.notes.concat(action.payload),
+      };
     default:
       return state;
   }
