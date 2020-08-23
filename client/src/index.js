b6347ab9c6b08847aa0b2903a525382a8b283569
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-23 10:03:41 -0500

    Organize imports on top level index.js file.

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 
 ReactDOM.render(
