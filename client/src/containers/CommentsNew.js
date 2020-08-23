aa9ce044241f1bd7ee5d5847402dafc933c9de09
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-22 19:21:25 -0500

    Update formatting on CommentsNew component.

--- client/src/containers/CommentsNew.js
+++ client/src/containers/CommentsNew.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {updateNote} from '../actions';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { withRouter } from 'react-router-dom';
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import { bindActionCreators } from 'redux';
+import { updateNote } from '../actions';
 
 class CommentsNew extends Component {
   constructor(props) {
@@ -17,58 +17,71 @@ class CommentsNew extends Component {
 
   toggle() {
     this.setState({
-      modal: !this.state.modal
+      modal: !this.state.modal,
     });
   }
 
-  handleOnSubmit = event => {
+  handleOnSubmit = (event) => {
     event.preventDefault();
-    const { updateNote , history } = this.props
-    updateNote(this.props.id, this.props.name, this.props.description, this.props.link, this.state.comments);
+    const { updateNote, history } = this.props;
+    updateNote(
+      this.props.id,
+      this.props.name,
+      this.props.description,
+      this.props.link,
+      this.state.comments
+    );
     history.push('/notes/' + this.props.id);
-    this.toggle()
-  }
+    this.toggle();
+  };
 
-  handleOnChange = event => {
+  handleOnChange = (event) => {
     this.setState({
-      comments: event.target.value
+      comments: event.target.value,
     });
-  }
+  };
 
   render() {
     return (
       <div>
-        <Button color="primary" onClick={this.toggle}>Add Comment</Button>
-          <Modal isOpen={this.state.modal} toggle={this.toggle} >
-            <ModalHeader toggle={this.toggle}>{this.props.name}</ModalHeader>
-            <ModalBody>
-              <form onSubmit={this.handleOnSubmit} >
-                <div className="form-group">
-                  <input
-                    type="text"
-                    name="comments"
-                    className="form-control"
-                    onChange={this.handleOnChange} />
-                </div>
-                  <button
-                    type="submit"
-                    className="btn btn-primary"
-                    >Add Comment</button>
-              </form>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-            </ModalFooter>
-          </Modal>
+        <Button color="primary" onClick={this.toggle}>
+          Add Comment
+        </Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>{this.props.name}</ModalHeader>
+          <ModalBody>
+            <form onSubmit={this.handleOnSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="comments"
+                  className="form-control"
+                  onChange={this.handleOnChange}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Add Comment
+              </button>
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="secondary" onClick={this.toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
   }
-};
+}
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    updateNote: updateNote
-  }, dispatch);
+  return bindActionCreators(
+    {
+      updateNote: updateNote,
+    },
+    dispatch
+  );
 };
 
 export default connect(null, mapDispatchToProps)(withRouter(CommentsNew));
