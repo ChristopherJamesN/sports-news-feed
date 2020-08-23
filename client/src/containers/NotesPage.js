f6014ea7e1e80b82452d5f3724a2024a9e4628c0
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-23 09:52:03 -0500

    Update formatting on NotesPage component.

--- client/src/containers/NotesPage.js
+++ client/src/containers/NotesPage.js
@@ -1,41 +1,48 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { getNotes } from '../actions';
 import NotesList from '../components/NotesList';
 import NotesShow from './NotesShow';
-import { getNotes } from '../actions';
-import { bindActionCreators } from 'redux';
 
 class NotesPage extends Component {
-
   componentDidMount() {
     this.props.getNotes();
   }
 
   render() {
     return (
-    <div>
-      <Switch>
-        <Route path={`${this.props.match.url}/:noteId`} component={NotesShow}/>
-        <Route exact path={this.props.match.url} render={() => (
-          <div>
-            <h3>Please select a story from the list.</h3>
-            <NotesList notes={this.props.notes} />
-          </div>
-        )}/>
-      </Switch>
-    </div>
-  )}
+      <div>
+        <Switch>
+          <Route
+            path={`${this.props.match.url}/:noteId`}
+            component={NotesShow}
+          />
+          <Route
+            exact
+            path={this.props.match.url}
+            render={() => (
+              <div>
+                <h3>Please select a story from the list.</h3>
+                <NotesList notes={this.props.notes} />
+              </div>
+            )}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
   return {
-    notes: state.notesReducer.notes
+    notes: state.notesReducer.notes,
   };
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
-  return { getNotes: bindActionCreators(getNotes, dispatch) }
+  return { getNotes: bindActionCreators(getNotes, dispatch) };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NotesPage);
