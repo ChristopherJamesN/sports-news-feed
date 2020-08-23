ad95d2ed2edab19167ac23bb8d00144fb865951a
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-23 09:45:19 -0500

    Update formatting on Navigation component.

--- client/src/containers/Navigation.js
+++ client/src/containers/Navigation.js
@@ -1,71 +1,106 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, Nav, NavItem, NavbarToggler, NavbarBrand } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+} from 'reactstrap';
 
 class Navigation extends Component {
-
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-        isOpen: false
-      };
-    }
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
 
-    toggle() {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
-    }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
 
   render() {
     return (
-      <Navbar color='light' light expand="md" className="sticky-top">
-      <NavbarToggler onClick={this.toggle} />
-      <NavbarBrand href="/">Home</NavbarBrand>
-      <Collapse isOpen={this.state.isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-        <br></br>
-        <NavItem>
-          <NavLink to='/news' style={{ textDecoration: 'none' }} onClick={this.toggle}>&nbsp; News Feed &nbsp;</NavLink>
-        </NavItem>
-        <br></br>
+      <Navbar color="light" light expand="md" className="sticky-top">
+        <NavbarToggler onClick={this.toggle} />
+        <NavbarBrand href="/">Home</NavbarBrand>
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <br></br>
+            <NavItem>
+              <NavLink
+                to="/news"
+                style={{ textDecoration: 'none' }}
+                onClick={this.toggle}
+              >
+                &nbsp; News Feed &nbsp;
+              </NavLink>
+            </NavItem>
+            <br></br>
 
-        {!this.props.isLoggedIn ? (
-          <Nav navbar>
-            <NavItem>
-              <NavLink to='/signin' style={{ textDecoration: 'none' }} onClick={this.toggle}>&nbsp; Login &nbsp;</NavLink>
-            </NavItem>
-            <br></br>
-            <NavItem>
-              <NavLink to='/signup' style={{ textDecoration: 'none' }} onClick={this.toggle}>&nbsp; Sign Up &nbsp;</NavLink>
-            </NavItem>
-            <br></br>
+            {!this.props.isLoggedIn ? (
+              <Nav navbar>
+                <NavItem>
+                  <NavLink
+                    to="/signin"
+                    style={{ textDecoration: 'none' }}
+                    onClick={this.toggle}
+                  >
+                    &nbsp; Login &nbsp;
+                  </NavLink>
+                </NavItem>
+                <br></br>
+                <NavItem>
+                  <NavLink
+                    to="/signup"
+                    style={{ textDecoration: 'none' }}
+                    onClick={this.toggle}
+                  >
+                    &nbsp; Sign Up &nbsp;
+                  </NavLink>
+                </NavItem>
+                <br></br>
+              </Nav>
+            ) : (
+              <Nav navbar>
+                <NavItem>
+                  <NavLink
+                    to="/notes"
+                    style={{ textDecoration: 'none' }}
+                    onClick={this.toggle}
+                  >
+                    &nbsp; Saved Stories &nbsp;
+                  </NavLink>
+                </NavItem>
+                <br></br>
+                <NavItem>
+                  <NavLink
+                    to="/signout"
+                    style={{ textDecoration: 'none' }}
+                    onClick={this.toggle}
+                  >
+                    &nbsp; Sign Out &nbsp;
+                  </NavLink>
+                </NavItem>
+                <br></br>
+              </Nav>
+            )}
           </Nav>
-          ) : (
-          <Nav navbar>
-            <NavItem>
-              <NavLink to='/notes' style={{ textDecoration: 'none' }} onClick={this.toggle}>&nbsp; Saved Stories &nbsp;</NavLink>
-            </NavItem>
-            <br></br>
-            <NavItem>
-              <NavLink to='/signout' style={{ textDecoration: 'none' }} onClick={this.toggle}>&nbsp; Sign Out &nbsp;</NavLink>
-            </NavItem>
-            <br></br>
-          </Nav>
-          )}
-
-        </Nav>
-      </Collapse>
+        </Collapse>
       </Navbar>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return { isLoggedIn: state.userReducer.isLoggedIn }
-}
+  return { isLoggedIn: state.userReducer.isLoggedIn };
+};
 
 export default connect(mapStateToProps)(Navigation);
