8e5e6162bb9667d8391f8443e552d40252b8ed99
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-23 10:03:05 -0500

    Organize imports on App component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import NotesPage from './containers/NotesPage';
-import NewsPage from './containers/NewsPage';
-import SignOutLink from './components/auth/SignOutLink';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
 import SignInForm from './components/auth/SignInForm';
+import SignOutLink from './components/auth/SignOutLink';
 import SignUpForm from './components/auth/SignUpForm';
+import Footer from './components/Footer';
 import Homepage from './containers/Homepage';
 import Navigation from './containers/Navigation';
-import Footer from './components/Footer';
+import NewsPage from './containers/NewsPage';
+import NotesPage from './containers/NotesPage';
 
 class App extends Component {
-
   render() {
     return (
       <Router>
         <div className="Nav-home">
-        <Navigation />
+          <Navigation />
           <div className="App">
-            <Route path="/notes" component={(
-              this.props.isLoggedIn === true
-                ? NotesPage
-                : SignInForm
-            )} />
+            <Route
+              path="/notes"
+              component={
+                this.props.isLoggedIn === true ? NotesPage : SignInForm
+              }
+            />
             <Route path="/news" component={NewsPage} />
             <Route path="/signout" component={SignOutLink} />
             <Route path="/signin" component={SignInForm} />
@@ -34,13 +34,14 @@ class App extends Component {
           </div>
         </div>
       </Router>
-  )}
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
   return {
-    isLoggedIn: state.userReducer.isLoggedIn
+    isLoggedIn: state.userReducer.isLoggedIn,
   };
-}
+};
 
 export default connect(mapStateToProps)(App);
