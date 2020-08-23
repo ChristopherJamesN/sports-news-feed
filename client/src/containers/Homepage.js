7e4b6fc2463c6843521aef2164275429c8d19232
ChristopherJamesN <nady.christopher@gmail.com>
2020-08-23 09:44:33 -0500

    Update formatting on Homepage component.

--- client/src/containers/Homepage.js
+++ client/src/containers/Homepage.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import IntroText from '../components/IntroText'
-import { Alert } from 'reactstrap';
 import { connect } from 'react-redux';
+import { Alert } from 'reactstrap';
+import IntroText from '../components/IntroText';
 
 class Homepage extends Component {
-    render() {
-      return(
-        <div>
-          <Alert color="info" isOpen={this.props.isLoggedIn} >You are logged in.</Alert>
-          <Alert color="info" isOpen={!this.props.isLoggedIn} >You are logged out.</Alert>
-          <IntroText />
-        </div>
-      )}
+  render() {
+    return (
+      <div>
+        <Alert color="info" isOpen={this.props.isLoggedIn}>
+          You are logged in.
+        </Alert>
+        <Alert color="info" isOpen={!this.props.isLoggedIn}>
+          You are logged out.
+        </Alert>
+        <IntroText />
+      </div>
+    );
   }
+}
 
-  const mapStateToProps = (state) => {
-    return {
-      isLoggedIn: state.userReducer.isLoggedIn
-    };
-  }
-
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.userReducer.isLoggedIn,
+  };
+};
 
 export default connect(mapStateToProps)(Homepage);
