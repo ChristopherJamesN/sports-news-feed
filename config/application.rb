4a6d1386e28e43a85aaf84a608cf38ae6ae2f167
ChristopherJamesN <nady.christopher@gmail.com>
2022-11-05 17:38:43 -0500

    Set config.load_defaults to 7.0
    This resolves warnings like
    DEPRECATION WARNING: Non-URL-safe CSRF tokens are deprecated. Use 6.1 defaults or above. (called from block (3 levels) in <class:Railtie> at /Users/christophernady/.rvm/gems/ruby-3.1.2/gems/actionpack-7.0.3.1/lib/action_controller/railtie.rb:80)
    when the server starts update via the `rake start` command.
    
    Also see https://stackoverflow.com/questions/72110385/url-safe-csrf-tokens-are-now-the-default-warning
    for more details and discussion.

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 module ReactReduxNbaNewsFeed
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 7.0
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
