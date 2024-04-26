77cd2d7382245b6ef91915aef4e190bc8a48e72a
ChristopherJamesN <nady.christopher@gmail.com>
2024-04-25 19:05:04 -0500

    Add slickdeals.net to `EXCLUDED_DOMAINS`
    Also remove TODO about extratv stories being returned from the API, since I have not seen this behavior in a few weeks, so it might be fixed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,9 @@ require 'net/http'
 class ApplicationController < ActionController::Base
   include Knock::Authenticable
 
-  # TODO: Even with extratv.com in the EXCLUDED_DOMAINS, the api is still returning stories from extratv.com.
-  # For example see https://mail.google.com/mail/u/0/#label/Nady+Analytics/FMfcgzGxSbqlSJdJNRNvhqdBQLVZJJWR.
   EXCLUDED_DOMAINS = 'thegatewaypundit.com,extratv.com,biztoc.com,dailycaller.com,rlsbb.ru,thefutoncritic.com,'\
   'drunkenstepfather.com,egotasticsports.com,zacjohnson.com,smartbitchestrashybooks.com,rlsbb.cc,'\
-  'bestadsontv.com,cscoblogs-prod-17bj.appspot.com,sarkarinaukriblog.com,cnet.com'.freeze
+  'bestadsontv.com,slickdeals.net,cscoblogs-prod-17bj.appspot.com,sarkarinaukriblog.com,cnet.com'.freeze
 
   def fallback_index_html
     render file: '../client/public/index.html'
