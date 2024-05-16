0e406120dca04b06edc093055e1fb441983420e8
ChristopherJamesN <nady.christopher@gmail.com>
2024-05-15 19:30:13 -0500

    Bump time that articles must have been published before to two days from one day
    Also remove a `TODO` that I think has been addressed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ require 'net/http'
 class ApplicationController < ActionController::Base
   include Knock::Authenticable
 
-  # TODO: Remove cnet.com from search results.
-  # cnet.com is still showing up in the responses from newsapi.org, for example see this email:
-  # https://mail.google.com/mail/u/0/#search/send+twee/FMfcgzGxTFdxlxhnxRmgwFzStmPKVjfn. It should not
-  # be included in responses since it is included in EXCLUDED_DOMAINS.
-  # You can also observe this by running:
-  # curl "https://rails-news-feed-jt3432sekq-uc.a.run.app/retrieve_news.json?searchTerm=sports"  | jq '.articles | map(select(.url | contains("cnet.com")))'
   EXCLUDED_DOMAINS = 'thegatewaypundit.com,extratv.com,biztoc.com,dailycaller.com,rlsbb.ru,thefutoncritic.com,'\
   'screenrant.com,drunkenstepfather.com,egotasticsports.com,zacjohnson.com,smartbitchestrashybooks.com,rlsbb.cc,'\
   'bestadsontv.com,slickdeals.net,cscoblogs-prod-17bj.appspot.com,sarkarinaukriblog.com,cnet.com'.freeze
@@ -23,13 +17,13 @@ class ApplicationController < ActionController::Base
   end
 
   def fetch_news_from_cache_or_api(search_term)
-    # Don't search for articles that were published more than one day ago.
+    # Don't search for articles that were published more than two days ago.
     # Proof from irb:
     # 3.1.2 :004 > Time.now
     # => 2024-05-14 19:14:24.173067 -0500
     # 3.1.2 :005 > Time.now - 86_400
     # => 2024-05-13 19:14:26.433881 -0500
-    t = Time.now - 86_400
+    t = Time.now - (86_400 * 2)
     formatted_date = t.strftime('%F')
     Rails.cache.fetch("/retrieve_news?searhTerm=#{search_term}", expires_in: 6.hours) do
       news_api_request_uri = "https://newsapi.org/v2/everything?q=#{search_term}&language=en&from=#{formatted_date}&excludeDomains=#{EXCLUDED_DOMAINS}&apiKey=#{Rails.application.credentials.news[:api_key]}"
